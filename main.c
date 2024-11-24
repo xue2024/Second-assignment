a3a5be034ff762dc4250e46157e4ea14e415cd96
xue2024 <lixiangsu_2024@qq.com>
2024-11-24 16:45:12 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
 //请在下方输入你的代码：
+#include "stdio.h"
+int main()
+{
+  int i,j;
+for(i=1;i<=9;i++)
+  {
+  for(j=i;j<=i;j++)
+    printf("%d*%d=%-4d",j,i,i*j);
+    printf("\n");
+  
+  }
+  
+for(int i=9;i>0;i--)
+  {
+    for(int j=i;j>0;j--)
+      {
+         printf("%d*%d=%4d",j,i,i*j);
+        
+      }
+       printf("\n")
+
+
+    return 0;
+  }
+
+
+
+
+
+
+  
+  return 0;
+}
+
+
+
+
